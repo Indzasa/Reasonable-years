4a42e5a21ae29ae46d696f65ea698e7562212cc6
indzasa <indzasa@gmail.com>
2020-03-11 20:24:57 +0300

    Standard -> Online
    This change should change calendar of stadard game speed to quick.

---
+++ Reasonable years/DietyCalendar.sql
@@ -0,0 +1,18 @@
+-- DietyCalendar
+-- Author: Indzasa
+-- DateCreated: 3/11/2020 8:14:05 PM
+--------------------------------------------------------------
+
+DELETE FROM GameSpeed_Turns;
+
+INSERT INTO GameSpeed_Turns
+(GameSpeedType,	MonthIncrement, TurnsPerIncrement)
+VALUES
+("GAMESPEED_STANDARD",	960,	35),
+("GAMESPEED_STANDARD",	600,	30),
+("GAMESPEED_STANDARD",	480,	20),
+("GAMESPEED_STANDARD",	240,	20),
+("GAMESPEED_STANDARD",	120,	30),
+("GAMESPEED_STANDARD",	48,		25),
+("GAMESPEED_STANDARD",	24,		60),
+("GAMESPEED_STANDARD",	12,		30),
